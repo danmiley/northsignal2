3bcd70dfaa425aa7a7787bab1e59de9f4b4a2dc5
danmiley <dan.miley@gmail.com>
2009-12-12 21:59:21 -0800

    adding a spell checker feature
    sample usage:
    
    http://localhost:3000/spell/index/foo.json?s=tirred%20of%20playinz%20this%20gamz

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,15 @@
 class SearchController < ApplicationController
-  
+  # example search : http://northsignal2.heroku.com/search/index/foo.json?s=ipho&app=ps 
+  # should yield a json payload, like this:
+  # ["ipho", ["iphone 3gs"],[],[]] 
   def index
     
       @search_terms = params[:s]
       @app = params[:app]
-      @result_array = ["hi","there", "world"]
-      @result_array = Search.all(:order => 'priority ASC', :limit => 10, :conditions => ["candidate LIKE ?", "%" + @search_terms +"%"]).map(&:candidate) # only return candidate strings
+      @result_array = ["hi","there", "world","hi","there", "world","hi","there", "world","hi","there", "world","hi","there", "world"]
+      # the mapping approach below came from http://snippets.dzone.com/posts/show/3901
+      @result_array = Search.all(:order => 'priority ASC', :limit => 10, :conditions => ["candidate LIKE ?", @search_terms +"%"]).map(&:candidate) # only return candidate strings
+            @result_array = ["hi","there", "world","hi","there", "world","hi","there", "world","hi","there", "world","hi","there", "world"] 
       render
   end
   
